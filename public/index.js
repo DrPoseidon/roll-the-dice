f9e370c76e9975cf889a29ce388e3fa9c5d16c9f
Антон Попов <a.popov.d@gmail.com>
2022-06-19 22:55:27 +0300

    have added some changes

--- public/index.js
+++ public/index.js
@@ -1,11 +1,13 @@
-const port = '45.130.151.203:9000/';
-// const port = window.location.host;
+const port = window.location.host;
 
 const ws = new WebSocket(`ws://${port}`);
 
 const divValue1 = document.querySelector('.value1');
 const divValue2 = document.querySelector('.value2');
 const helloDiv = document.querySelector('.hello');
+const rollBtn = document.querySelector('.roll-btn');
+const dicesDiv = document.querySelector('.dices');
+
 const images = [
   'one',
   'two',
@@ -25,26 +27,44 @@ ws.onclose = () => {
 
 ws.onmessage = response => {
   const json = JSON.parse(response.data);
-
   helloDiv.style.display = 'none';
-
   divValue1.src = `./public/images/${images[json[0] - 1]}.png`;
   divValue2.src = `./public/images/${images[json[1] - 1]}.png`;
 };
 
 const rollTheDice = () => {
-  const json = {
-    0: getRandomArbitrary(),
-    1: getRandomArbitrary()
-  };
+  rollBtn.setAttribute('disabled', '');
+  rollBtn.classList.add('disabled');
+  dicesDiv.style.display = 'flex';
 
+  let json = {};
   helloDiv.style.display = 'none';
 
-  ws.send(JSON.stringify(json));
+  let timerId = setInterval(() => {
+    json = {
+      0: getRandomArbitrary(),
+      1: getRandomArbitrary()
+    };
+
+    setSrc(json[0], json[1]);
+  }, 250);
+
+
+  setTimeout(() => {
+    clearInterval(timerId);
+    ws.send(JSON.stringify(json));
+    rollBtn.removeAttribute('disabled');
+    rollBtn.classList.remove('disabled');
+  }, 2500);
 };
 
 const getRandomArbitrary = (min = 1, max = 6) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const setSrc = (first, second) => {
+  divValue1.src = `./public/images/${images[first - 1]}.png`;
+  divValue2.src = `./public/images/${images[second - 1]}.png`;
 };
